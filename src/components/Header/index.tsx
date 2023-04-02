9bcef5523d5aa5556e062564a563658a73dcd1fe
Marian <dudychmarian@gmail.com>
2023-04-03 00:01:25 +0100

    Add router

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 import styles from "./Header.module.scss";
 import logo from "../../assets/image/logo.svg";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <img src={logo} alt="Logo" />
+      <Link to="/">
+        <img src={logo} alt="Logo" />
+      </Link>
     </header>
   );
 };
