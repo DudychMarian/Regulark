0addac088ef534c29fee1a107988c169f1f8d530
Marian <dudychmarian@gmail.com>
2023-04-03 23:29:16 +0100

    Allow search by tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { NotFound } from "./pages/NotFound";
+import { Home } from "./pages/Home";
 
 import patternsCollection from "./assets/patterns.json";
 
 import styles from "./App.module.scss";
-import { Card } from "./components/Card";
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  const location = useLocation().search;
+
+  useEffect(() => {
+    let value = new URLSearchParams(location).get("search");
+    setSearch(value ? value : "");
+  }, [location]);
+
+  useEffect(() => {
+    setSearch(searchValue);
+  }, [searchValue]);
+
+  const result = patternsCollection.filter((obj) =>
+    obj.tags.toLocaleLowerCase().includes(search.toLocaleLowerCase().trim())
+  );
+
   return (
     <div className={styles.container}>
-      <Header />
-      <main className={styles.wrapper}>
-        {patternsCollection.map((obj) => (
-          <Card
-            key={obj.title}
-            title={obj.title}
-            description={obj.description}
-            tags={obj.tags}
-            placeholder={obj.placeholder}
-            pattern={obj.pattern}
-          />
-        ))}
-      </main>
+      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
+      <main className={styles.wrapper}>{result.length ? <Home result={result} /> : <NotFound />}</main>
       <Footer />
     </div>
   );
